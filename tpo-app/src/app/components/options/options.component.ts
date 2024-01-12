941d5a7136f2bfe581f1b5abdc13c94326d6c2d8
NejcBrnelic <96769900+NejcBrnelic@users.noreply.github.com>
2024-01-12 16:27:17 +0100

    Redirect added za odjavo

--- tpo-app/src/app/components/options/options.component.ts
+++ tpo-app/src/app/components/options/options.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-options',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './options.component.css'
 })
 export class OptionsComponent {
+  constructor(private router: Router) {}
 
+  logout() {
+    
+    this.router.navigate(['/join-room']);
+  }
 }
