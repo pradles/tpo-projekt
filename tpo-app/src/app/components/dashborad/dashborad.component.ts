06f496ae51f240e95da62250a4aee2d7d70345a0
NejcBrnelic <96769900+NejcBrnelic@users.noreply.github.com>
2024-01-13 18:04:38 +0100

    Graf za dashboard

--- tpo-app/src/app/components/dashborad/dashborad.component.ts
+++ tpo-app/src/app/components/dashborad/dashborad.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { CostsService } from '../../services/costs.service';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dashborad',
   templateUrl: './dashborad.component.html',
-  styleUrl: './dashborad.component.css'
+  styleUrls: ['./dashborad.component.css']
 })
-export class DashboradComponent {
+export class DashboradComponent implements OnInit, AfterViewInit {
+  userExpensesChart: any;
+  userTotalExpenses: { userId: number; totalExpense: number }[] = [];
 
+  constructor(private costsService: CostsService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.loadExpensesAndCreateChart();
+  }
+
+  ngAfterViewInit(): void {
+    // Chart creation can remain in ngAfterViewInit
+    this.createChart();
+  }
+
+  loadExpensesAndCreateChart(): void {
+    this.userTotalExpenses = this.costsService.calculateTotalExpenses();
+    this.createChart();
+  }
+
+  createChart(): void {
+    const userNames = this.costsService.users.map(user => user.name);
+
+    this.userExpensesChart = new Chart('userExpensesChart', {
+      type: 'pie',
+      data: {
+        labels: userNames,
+        datasets: [
+          {
+            data: this.userTotalExpenses.map(user => user.totalExpense),
+            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    });
+  }
+
+  getUserName(id: number): string | undefined {
+    return this.costsService.getUserName(id);
+  }
+
+  
+  updateView(): void {
+    this.cdr.detectChanges();
+  }
 }
